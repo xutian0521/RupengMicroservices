78efc7c75e07ac79e6cc89fcb464ecb585242635
xutian <xutian0521@qq.com>
2018-08-20 15:13:45 +0800

    简单的Ocelot 使用

---
+++ OcelotTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace OcelotTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseUrls("http://127.0.0.1:8888")
+            .ConfigureAppConfiguration((hostingContext, builder) => {
+                builder.AddJsonFile("configuration.json", false, true);
+            })
+                .UseStartup<Startup>();
+    }
+}
