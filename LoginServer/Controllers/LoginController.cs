7a57193b9333d34b0039b8ff19c1d07c75174134
xutian <xutian0521@qq.com>
2018-08-21 17:05:27 +0800

    Ocelot_Id2 第二版优化：独立登录服务器

---
+++ LoginServer/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> RequestToken(RequestTokenParam model)
+        {
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            dict["client_id"] = "clientPC1";
+            dict["client_secret"] = "123321";
+            dict["grant_type"] = "password";
+            dict["username"] = model.username;
+            dict["password"] = model.password;
+            //由登录服务器向IdentityServer发请求获取Token
+            using (HttpClient http = new HttpClient())
+            using (var content = new FormUrlEncodedContent(dict))
+            {
+                var msg = await http.PostAsync("http://127.0.0.1:9500/connect/token", content);
+                string result = await msg.Content.ReadAsStringAsync();
+                return Content(result, "application/json");
+            }
+        }
+    }
+}
---
+++ LoginServer/RequestTokenParam.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginServer
+{
+    public class RequestTokenParam
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
